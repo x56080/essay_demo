e032406828fb82da2030ce56ca2901bd67b94297
allenhu <thefivefire@gmail.com>
2013-11-18 16:12:13 +0800

    修改基本配置

--- essay_demo/manage.py
+++ essay_demo/manage.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-def main():
+if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "essay_demo.settings")
 
     from django.core.management import execute_from_command_line
--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -11,10 +11,20 @@ from essay.tasks import deploy
 #from essay.tasks import nginx
 #from essay.tasks import supervisor
 
+env.GIT_SERVER = 'https://github.com/'  # ssh地址只需要填：github.com
 env.PROJECT = 'essay_demo'
-#env.DEPLOY_HOST = '127.0.0.1'
-#env.DEPLOY_USER = 'username'
-#env.DEPLOY_PATH = '/opt/deploy/'
-#env.PROJECT_OWNER = 'EssayTech'
-#env.DEFAULT_BRANCH = 'master'
-#env.PYPI_INDEX = 'http://pypi.python.org/simple'
+env.BUILD_PATH = '~/buildspace'
+env.PROJECT_OWNER = 'EssayTech'
+env.DEFAULT_BRANCH = 'master'
+env.PYPI_INDEX = 'http://pypi.python.org/simple'
+
+######
+# deploy settings:
+env.PROCESS_COUNT = 2  #部署时启动的进程数目
+env.roledefs = {
+    'build': ['vagrant@127.0.0.1:2202'],  # 打包服务器配置
+    'dev': [],
+}
+
+env.VIRTUALENV_PREFIX = '/home/django/essay_demo'
+env.SUPERVISOR_CONF_TEMPLATE = os.path.join(PROJECT_ROOT, 'conf', 'supervisord.conf')
