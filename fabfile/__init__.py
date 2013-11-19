9474fea1c46bf44bbcdd60d21f9c27108e6d8e2b
allenhu <thefivefire@gmail.com>
2013-11-19 11:07:34 +0800

    添加双环境切换配置

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -23,8 +23,36 @@ env.PYPI_INDEX = 'http://127.0.0.1:10110/simple'
 env.PROCESS_COUNT = 2  #部署时启动的进程数目
 env.roledefs = {
     'build': ['vagrant@127.0.0.1:2202'],  # 打包服务器配置
-    'dev': [],
+    'dev': ['vagrant@127.0.0.1:2201'],
 }
 
-env.VIRTUALENV_PREFIX = '/home/django/essay_demo'
+env.VIRTUALENV_PREFIX = '/home/vagrant/essay_demo'
 env.SUPERVISOR_CONF_TEMPLATE = os.path.join(PROJECT_ROOT, 'conf', 'supervisord.conf')
+
+#根据工程确定项目编号, 不同环境保证监听不同的端口
+PROJECT_NUM = 88
+env.VENV_PORT_PREFIX_MAP = {
+    'a': '%d0' % PROJECT_NUM,
+    'b': '%d1' % PROJECT_NUM,
+    'c': '%d2' % PROJECT_NUM,
+    'd': '%d3' % PROJECT_NUM,
+    'e': '%d4' % PROJECT_NUM,
+    'f': '%d5' % PROJECT_NUM,
+    'g': '%d6' % PROJECT_NUM,
+    'h': '%d7' % PROJECT_NUM,
+    'i': '%d8' % PROJECT_NUM,
+}
+# nginx配置
+###########
+
+# 切换nginx环境
+env.NGINX_SWITCH_CONF = '/etc/nginx/extra/nginx.conf'
+# nginx启动配置文件
+env.NGINX_CONF = '/etc/nginx/extra/nginx.conf'
+env.NGINX_BIN = '/usr/sbin/nginx'
+
+env.SWITCH_VERSION_MAP = {
+    'a': 'upstreamA',
+    'b': 'upstreamB',
+}
+
